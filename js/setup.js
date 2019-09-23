929141ef328bd75ff3f64054acfc3427d3927aba
frenby52 <aakytin@mail.ru>
2019-09-23 16:04:33 +0300

    fix coat color

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,7 @@
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT_COLORS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_QUANTITY = 4;
 var setupBlock = document.querySelector('.setup');
@@ -59,7 +59,12 @@ var activateSetup = function () {
   setupSimilarBlock.classList.remove('hidden');
 };
 
+var activateSetupSimilar = function () {
+  setupSimilarBlock.classList.remove('hidden');
+};
+
 activateSetup();
+activateSetupSimilar();
 
 var createWizardElement = function (data) {
   var wizardElement = wizardTemplate.cloneNode(true);
