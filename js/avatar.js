9e494619ae4786eb97a1c105a8f3e83abf116a5b
frenby52 <aakytin@mail.ru>
2019-10-14 16:37:20 +0300

    add avatar upload preview

---
+++ js/avatar.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var fileChooser = document.querySelector('.upload input[type=file]');
+  var preview = document.querySelector('.setup-user-pic');
+
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    if (file) {
+      var fileName = file.name.toLowerCase();
+
+      var matches = FILE_TYPES.some(function (it) {
+        return fileName.endsWith(it);
+      });
+
+      if (matches) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+          preview.src = reader.result;
+        });
+
+        reader.addEventListener('error', function () {
+          window.message.showError('Произошла ошибка загрузки аватара');
+        });
+
+        reader.readAsDataURL(file);
+      } else {
+        window.message.showError('Неверный формат файла аватара');
+      }
+    }
+  });
+})();
