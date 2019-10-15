539f4903837509706a50c2e94476231332b91c86
frenby52 <aakytin@mail.ru>
2019-10-15 16:00:36 +0300

    add removeErrorMessage

--- js/avatar.js
+++ js/avatar.js
@@ -18,15 +18,18 @@
         var reader = new FileReader();
 
         reader.addEventListener('load', function () {
+          window.message.removeErrorMessage();
           preview.src = reader.result;
         });
 
         reader.addEventListener('error', function () {
+          window.message.removeErrorMessage();
           window.message.showError('Произошла ошибка загрузки аватара');
         });
 
         reader.readAsDataURL(file);
       } else {
+        window.message.removeErrorMessage();
         window.message.showError('Неверный формат файла аватара');
       }
     }
